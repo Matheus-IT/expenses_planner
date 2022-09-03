b392562bcdedb31cb7f6dcb0e1eb409469dc2148
matheus costa <matheuscosta.tech@gmail.com>
2022-09-03 15:47:59 -0300

    Organizing my stuff

---
+++ lib/dummy_data.dart
@@ -0,0 +1,40 @@
+import 'models/transaction_model.dart';
+
+final dummyData = [
+  TransactionModel(
+    id: 't1',
+    title: 'New Shoes',
+    amount: 69.99,
+    date: DateTime.now(),
+  ),
+  TransactionModel(
+    id: 't2',
+    title: 'Weekly Groceries',
+    amount: 16.53,
+    date: DateTime.now(),
+  ),
+  TransactionModel(
+    id: 't3',
+    title: 'Weekly Groceries',
+    amount: 16.53,
+    date: DateTime.now(),
+  ),
+  TransactionModel(
+    id: 't4',
+    title: 'Weekly Groceries',
+    amount: 16.53,
+    date: DateTime.now(),
+  ),
+  TransactionModel(
+    id: 't5',
+    title: 'Weekly Groceries',
+    amount: 16.53,
+    date: DateTime.now(),
+  ),
+  TransactionModel(
+    id: 't6',
+    title: 'Weekly Groceries',
+    amount: 16.53,
+    date: DateTime.now(),
+  ),
+];
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:uuid/uuid.dart';
+
+import '../components/add_transaction_area.dart';
+import '../components/transaction_item.dart';
+import '../dummy_data.dart';
+import '../models/transaction_model.dart';
+
+class HomePage extends StatefulWidget {
+  HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final List<TransactionModel> transactions = dummyData;
+
+  void handleAddTransaction(String title, String amount) {
+    final newTransaction = TransactionModel(
+      title: title,
+      amount: double.parse(amount),
+      date: DateTime.now(),
+      id: const Uuid().v4(),
+    );
+    setState(() => transactions.add(newTransaction));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter App'),
+        // actions: [
+        //   IconButton(onPressed: () {}, icon: const Icon(Icons.add)),
+        // ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            const SizedBox(
+              width: double.infinity,
+              child: Card(
+                elevation: 5,
+                child: Text('Card 1'),
+              ),
+            ),
+            AddTransactionArea(onSubmit: handleAddTransaction),
+            SizedBox(
+              height: 350,
+              child: ListView.builder(
+                itemBuilder: (ctx, index) =>
+                    TransactionItem(transactionModel: transactions[index]),
+                itemCount: transactions.length,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
